590b41fc053afdba8dcdcd862c43dff086d896d8
KyleAxley <kanderson7645@yahoo.com>
2022-07-20 01:05:02 -0400

    added func to initialize app/more questions

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const inquirer = require("inquirer");
+const { prompt } = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        name: "project_name",
-        message: "What is your projects name? (required)",
-        validate: (nameInput) => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your projects name!");
-                return false;
-            }
-        },
+  {
+    type: "input",
+    name: "project_name",
+    message: "What is your projects name? (required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter your projects name!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "project_description",
-        message: "Please briefly describe your project",
-        validate: (nameInput) => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log("Please add a bried description of your project.");
-                return false;
-            }
-        },
+  },
+  {
+    type: "input",
+    name: "project_description",
+    message: "Briefly describe your project",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please add a brief description of your project!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "project_description",
-        message: "Please briefly describe your project",
-        validate: (nameInput) => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log("Please add a bried description of your project.");
-                return false;
-            }
-        },
+  },
+  {
+    type: "input",
+    name: "project_Installation",
+    message: "What are the steps required to install your project?",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log(
+          "Please provide installation instructions!"
+        );
+        return false;
+      }
     },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What are the instructions for using your project?",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log(
+          "Please provide instructions for using your project."
+        );
+        return false;
+      }
+    },
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  const answers = await prompt(questions);
+}
 
 // Function call to initialize app
 init();
